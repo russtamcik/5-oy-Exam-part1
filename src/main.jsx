20898d55df20b79f6145a6fdd8891eafe693e03a
russtamcik <abbasovr455@gmail.com>
2023-09-26 02:16:36 +0500

    togirlandi

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
-import "swiper/css";
 import "./index.css";
 import AuthContextProvider from "./contexts/AuthContext.jsx";
 
